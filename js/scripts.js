1571e3ac0c30e3ea4bc871f73c462dfbe5ac3aee
Claudio-Pri <priv.claudio@gmail.com>
2024-09-03 16:11:33 +0200

    stampa lista in pagina

--- js/scripts.js
+++ js/scripts.js
@@ -16,20 +16,39 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Ciao Vue!',
             url: 'https://flynn.boolean.careers/exercises/api/random/mail',
-            email: ''
+            email: '',
+            emailList: [],
         }
     },
     created() {
-        axios
-            //url memorizzato in data come stringa per evitare il problema del doppio slash
-            .get(this.url)
-            .then((rispostaApi) => {
-                // console.log(rispostaApi.data.response);
-                this.email = rispostaApi.data.response;
-                console.log(this.email);
-            })
+
+        for (let i = 0; i < 10; i++) {
+            axios
+                //url memorizzato in data come stringa per evitare il problema del doppio slash
+                .get(this.url)
+                .then((rispostaApi) => {
+                    // console.log(rispostaApi.data.response);
+                    this.email = rispostaApi.data.response;
+
+                    console.log('email casuale:', this.email);
+
+
+                    this.emailList.push(this.email);
+
+                    console.log('array email:', this.emailList);
+
+
+                });
+        }
+        // axios
+        //     //url memorizzato in data come stringa per evitare il problema del doppio slash
+        //     .get(this.url)
+        //     .then((rispostaApi) => {
+        //         // console.log(rispostaApi.data.response);
+        //         this.email = rispostaApi.data.response;
+        //         console.log(this.email);
+        //     });
 
     }
 
